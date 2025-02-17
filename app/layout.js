eca42c6c4b4fef113fe0b31aa1f92d3fc5a82e12
Hardik Agarwal <xfinitynexus766@gmail.com>
2025-02-17 14:28:08 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import SessionWrapper from "../components/SessionWrapper";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,11 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"
-      >
-        {children}
+      <body className="bg-gradient-to-br from-[#000] via-[#63e] to-[#000] min-h-screen">
+        <SessionWrapper>
+          {children}
+        </SessionWrapper>
       </body>
     </html>
   );
